7abedec3a38f8b1c93eca98a24a592fb4ee93f74
ThomasHoest <zoglub@gmail.com>
2012-10-26 19:01:33 +0200

    First commit

---
+++ Source/AiLibrary/GeneticAlgorithm/GA.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AiLibrary.GeneticAlgorithm
+{
+  public interface IGAFunctions
+  {
+    double CalculateFitness(BitArray data);
+    int PopulationSize { get; }
+    int NumberOfChromosones { get; }
+    double MutationRate { get; }
+    double CrossOverRate { get; }
+  }
+
+  public class Chromosome
+  {
+    public Chromosome()
+    {
+    }
+
+    public Chromosome(int length)
+    {
+      Data = new BitArray(length);
+      Random rand = new Random();
+      for(int i = 0; i<length; i++)
+        Data.Set(i,rand.Next(2) == 1);        
+    }
+
+    public BitArray Data { get; set; } 
+    public double Fitness { get; set; }
+
+  }
+
+  public class GA
+  {
+    public Chromosome Best { get; set; }
+    public double BestFit { get; set; }
+    public int Generation { get; set; }
+    private Random m_Rand = new Random();
+    IGAFunctions m_Functions;
+    private bool m_DoRun;
+    public List<Chromosome> Chromosomes { get; set; }
+    public event EventHandler OnNewGeneration;
+    public event EventHandler OnBestFitChanged;
+
+    public GA(IGAFunctions functions)
+    {
+      m_Functions = functions;
+      Chromosomes = new List<Chromosome>();
+
+      for (int i = 0; i < functions.PopulationSize; i++)
+      {
+        Chromosome chromosome = new Chromosome(functions.NumberOfChromosones);
+        chromosome.Fitness = m_Functions.CalculateFitness(chromosome.Data);
+        Chromosomes.Add(chromosome);
+      }
+    }
+
+
+    private void MakeNewGeneration()
+    {
+      List<Chromosome> children = new List<Chromosome>();
+
+      while (children.Count < m_Functions.PopulationSize)
+      {
+        Chromosome mum = SelectChromosome();
+        Chromosome dad = SelectChromosome();
+
+        bool doCrossOver = TossTheDice(m_Functions.CrossOverRate);
+
+        Chromosome child1 = doCrossOver ? CrossOver(mum, dad) : mum;
+        Chromosome child2 = doCrossOver ? CrossOver(dad, mum) : dad;
+
+        Mutate(child1);
+        Mutate(child2);
+
+        child1.Fitness = m_Functions.CalculateFitness(child1.Data);
+        child2.Fitness = m_Functions.CalculateFitness(child2.Data);
+
+        children.Add(child1);
+        children.Add(child2);
+
+        UpdateBestFit(child1);
+        UpdateBestFit(child2);
+
+      }
+
+      Chromosomes = children;
+    }
+
+    private void UpdateBestFit(Chromosome c)
+    {
+      if (c.Fitness > BestFit)
+      {
+        BestFit = c.Fitness;
+        Best = c;
+        if (OnBestFitChanged != null)
+          OnBestFitChanged(this, EventArgs.Empty);
+      }
+    }
+
+    private void Mutate(Chromosome c)
+    {
+      for (int i = 0; i < c.Data.Length; i++)
+      {
+        if (TossTheDice(m_Functions.MutationRate))
+          c.Data.Set(i, !c.Data[i]);
+      }
+    }
+
+    private bool TossTheDice(double p)
+    {
+      double val = m_Rand.Next(10000)/(double)10000;
+      return p > val;
+    }
+
+    private Chromosome CrossOver(Chromosome p1, Chromosome p2)
+    {
+      Chromosome c = new Chromosome();
+      c.Data = p1.Data.Clone() as BitArray;
+      int index = m_Rand.Next(c.Data.Length);
+      for (int i = 0; i < index; i++)
+        c.Data.Set(i, p2.Data[i]);
+      return c;
+    }
+
+    private Chromosome SelectChromosome()
+    {
+      double totalFit = Chromosomes.Sum(g => g.Fitness);
+      double fitnessIndex = 0;
+      Random rand = new Random();
+      double randomFitness = rand.Next((int)totalFit*100) / 100;
+
+      for (int i = 0; i < Chromosomes.Count; i++)
+      {
+        Chromosome c = Chromosomes[i];
+        fitnessIndex += c.Fitness;
+        if (fitnessIndex > randomFitness)
+          return c;
+      }
+
+      throw new InvalidOperationException("Should have found a genome");
+    }    
+
+    public void Start()
+    {
+      m_DoRun = true;
+      ThreadPool.QueueUserWorkItem(s=>
+        {
+          while (m_DoRun)
+          {
+            MakeNewGeneration();
+            Generation++;
+            if(OnNewGeneration != null)
+              OnNewGeneration(this, EventArgs.Empty);
+          }
+        });
+    }
+  }
+}
---
+++ Source/AiTests/GATest.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AiLibrary.GeneticAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Threading;
+using System.Diagnostics;
+
+namespace AiTests
+{
+  [TestClass]
+  public class GATest
+  {
+    struct Schedule
+    {
+      public List<Slot> Slots { get; set; }
+      public double BoxUsage { get; set; }
+    }
+
+    class Box
+    {
+      public int Id { get; set; }
+      public int Size { get; set; }
+    }
+
+    class Slot
+    {
+      public Slot()
+      {
+        Boxes = new List<Box>();
+      }
+      public int Size { get; set; }
+      public List<Box> Boxes { get; set; }
+      public double PercentFilled { get { return Boxes.Sum(b => b.Size) / (double)Size; } }
+      public int Fill { get { return Boxes.Sum(b => b.Size); } }
+    }
+
+    class BestFitFunctions : IGAFunctions
+    {
+      List<Box> m_Boxes = new List<Box>();
+      int m_SlotSize;
+      int m_BoxIndexBitSize;
+      int m_SlotIndexBitSize;
+      int m_NumSlots;
+      private List<Slot> m_Slots;
+      
+      public BestFitFunctions(int maxBoxSize, int slotSize, int numBoxes)
+      {
+        m_SlotSize = slotSize;
+        Random rand = new Random();
+        for (int i = 0; i < numBoxes; i++)
+        {
+          m_Boxes.Add(new Box() { Size = rand.Next(maxBoxSize), Id = i });
+        }
+        double sumOfBoxes = m_Boxes.Sum(b => b.Size);
+        double slots =  sumOfBoxes / slotSize;
+        m_NumSlots = (int)Math.Ceiling(slots);
+        m_SlotIndexBitSize = Convert.ToString(m_NumSlots-1, 2).Length;
+        m_BoxIndexBitSize = Convert.ToString(numBoxes-1, 2).Length;
+
+        m_Slots = new List<Slot>();
+        for (int i = 0; i < m_NumSlots; i++)
+          m_Slots.Add(new Slot() { Size = m_SlotSize });
+      }
+
+      
+
+      public Schedule Decode(BitArray data)
+      {        
+        foreach (Slot s in m_Slots)
+          s.Boxes.Clear();
+
+        bool[] boxesSelected = new bool[m_Boxes.Count];
+
+        bool[] boxIndex = new bool[m_BoxIndexBitSize];
+        bool[] slotIndex = new bool[m_SlotIndexBitSize];
+
+        for (int i = 0; i < data.Length; )
+        {
+          for (int b = 0; b < m_BoxIndexBitSize; b++)
+          {
+            boxIndex[b] = data[i + b];
+
+          }
+          i += m_BoxIndexBitSize;
+
+          for (int s = 0; s < m_SlotIndexBitSize; s++)
+          {
+            slotIndex[s] = data[i + s];
+
+          }
+          i += m_SlotIndexBitSize;
+
+          int bi = BoolArrayToInt(boxIndex);
+          int si = BoolArrayToInt(slotIndex);
+
+          if (si >= m_Slots.Count || bi >= m_Boxes.Count)
+            continue;
+          boxesSelected[bi] = true;
+          m_Slots[si].Boxes.Add(m_Boxes[bi]);
+        }
+
+        Schedule sc = new Schedule();
+        sc.Slots = m_Slots;
+        sc.BoxUsage = boxesSelected.Where(b => b).ToArray().Length / (double)m_Boxes.Count;
+        return sc;
+      }
+
+      public double CalculateFitness(System.Collections.BitArray data)
+      {
+        Decode(data);
+
+        double fitness = 0;
+        bool[] boxesSelected = new bool[m_Boxes.Count];
+        foreach (Slot s in m_Slots)
+        {
+          if (s.Boxes.Count == 0 )
+            continue;
+          if (s.PercentFilled > 1)
+            fitness -= (s.PercentFilled - 1);
+          else
+            fitness += s.PercentFilled;
+
+          foreach (Box b in s.Boxes)
+          {
+            if (boxesSelected[b.Id])
+            {
+              fitness -= 0.05;
+            }
+            else
+              boxesSelected[b.Id] = true;
+          }
+        }
+
+        fitness += (boxesSelected.Where(b => b).ToArray().Length / (double)m_Boxes.Count)*10;
+        if (fitness < 0)
+          return 0.001;
+        return fitness;
+      }
+
+      private int BoolArrayToInt(bool[] data)
+      {
+        int res = 0;
+        for (int i = 0; i < data.Length; i++)
+          res += data[i] ? (int)Math.Pow(2, i) : 0;
+        return res;
+      }
+
+      public int PopulationSize
+      {
+        get { return 200; }
+      }
+
+      public int NumberOfChromosones
+      {
+        get 
+        {          
+          return m_Boxes.Count*(m_BoxIndexBitSize + m_SlotIndexBitSize); 
+        }
+      }
+
+      public double MutationRate
+      {
+        get { return 0.001; }
+      }
+
+      public double CrossOverRate
+      {
+        get { return 0.7; }
+      }
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+      AutoResetEvent stopEvent = new AutoResetEvent(false); 
+      BestFitFunctions bs = new BestFitFunctions(2000, 4000, 8);
+      GA ga = new GA(bs);
+
+      ga.OnBestFitChanged += (s, e) =>
+      {
+        Debug.WriteLine(string.Format("New best fit {0}", ga.BestFit));
+        Schedule sc = bs.Decode(ga.Best.Data);
+        foreach(Slot sl in sc.Slots)
+          Debug.WriteLine(string.Format("Slot percent {0}", sl.PercentFilled));
+        Debug.WriteLine(string.Format("Box usage {0}", sc.BoxUsage));
+
+      };
+
+      ga.Start();
+
+      stopEvent.WaitOne(TimeSpan.FromMinutes(2));
+    }
+  }
+}
