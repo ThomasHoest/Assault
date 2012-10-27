dcd6edb1415947ec2db5cea7fec87b95398eb2a9
Thomas Høst Andersen <zoglub@gmail.com>
2012-10-27 09:06:00 +0200

    startet neural net

---
+++ Source/AiLibrary/NeuralNet/NeuralNet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiLibrary.NeuralNet
+{
+  public class NeuralNet
+  {
+
+
+  }
+
+  class Neuron
+  {
+    public List<Connections> Connections { get; set; }
+
+    public Neuron()
+    {
+      Connections = new List<Connections>();
+    }
+
+    public void AddConnection(Neuron n, double w)
+    {
+      Connections.Add(new Connections(){Target = n, Weigth = w});
+    }
+
+
+
+  }
+
+  class Connections
+  {
+    public double Weigth { get; set; }
+    public Neuron Target { get; set; }
+  }
+}
+
